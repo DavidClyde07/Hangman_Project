2fbb59403fe2716188e12d53931dcf5bce8b36cc
DavidClyde07 <davidclydechetty@gmail.com>
2020-08-11 23:09:23 +0000

    Added buttons with letters that you can click.

--- main.py
+++ main.py
@@ -45,11 +45,16 @@ RADIUS, GAP = 20, 15
 letters = []
 x_start = round((WIDTH - (RADIUS*2 + GAP) * 13 ) / 2 )
 y_start = 400 
+A = 65 #ASCII code for the letter A
+
+#Creating a font for the letters, comicsans is the font and 40 is the font size
+LETTER_FONT = pygame.font.SysFont("comicsans", 40)
+
 
 for i in range(26):
   x = x_start + GAP*2 + ((RADIUS*2 + GAP) * (i % 13))
   y = y_start + ((i // 13) * (GAP + RADIUS * 2))
-  letters.append([x,y])
+  letters.append([x,y, chr(A + i)]) #cast chr to get the ASCII charater with the code A+i, as i increments you get B , C etc...
 
 
 
@@ -62,13 +67,16 @@ for i in range(26):
 def draw() :
 
   window.fill(WHITE)
-
+  
+  #Drawing buttons
   for letter in letters :
-    x , y  = letter #Unpacking letter. Recall each element in looks like [x,y]
+    x , y, ltr  = letter #Unpacking letter. Recall each element in looks like [x,y, A+i]
     #drawing each cicle parameters are where, color, start pos, radius, line weight
     pygame.draw.circle(window, BLACK, (x, y), RADIUS, 3) 
-
-
+    text = LETTER_FONT.render(ltr, 1, BLACK)
+    window.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+  
+  
   window.blit(images[hangman_status], (100,150))
   pygame.display.update()
 
@@ -94,8 +102,14 @@ while run:
       run = False
     #Handel mouse clicks
     if event.type == pygame.MOUSEBUTTONDOWN:
-      pos = pygame.mouse.get_pos() #Note pygame coord sys starts at top left
-      print(pos)
+      x_mouse, y_mouse = pygame.mouse.get_pos() #Note pygame coord sys starts at top left
+      #print(pos)
+
+      for letter in letters:
+        x,y,ltr = letter
+        distance = math.sqrt((x-x_mouse)**2 + (y-y_mouse)**2)
+        if distance < RADIUS:
+          print(ltr)
 
 
 pygame.quit()
