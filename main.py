04403d4b2c257af92c5398827669b1b64528066e
DavidClyde07 <davidclydechetty@gmail.com>
2020-08-11 22:19:58 +0000

    Added draw function abd drew the circles for the buttons

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-############################################################
-# Exploring the python PyGame module by creating a simple  #
-# hangman game.                                            #
-############################################################
+####################################################################################
+'''Exploring the python PyGame module by creating a simple hangman game.'''
+####################################################################################
 
 import pygame
+import math
+
 
 pygame.init()  #Initializing the pygame module
 
-#####################################################################################
+####################################################################################
 '''Setting up the game display window'''
-#####################################################################################
+####################################################################################
 
 #Variable for the display, NOTE these are constants 
 WIDTH, HEIGHT = 800, 500
@@ -34,8 +35,44 @@ for i in range(7):
 '''Game Variables'''
 ####################################################################################
 hangman_status = 1 #tracks the player status hence which image to draw
-WHITE = (255,25,255)
+WHITE = (255,255,255)
 RED = (255,0,0)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+
+RADIUS, GAP = 20, 15
+
+letters = []
+x_start = round((WIDTH - (RADIUS*2 + GAP) * 13 ) / 2 )
+y_start = 400 
+
+for i in range(26):
+  x = x_start + GAP*2 + ((RADIUS*2 + GAP) * (i % 13))
+  y = y_start + ((i // 13) * (GAP + RADIUS * 2))
+  letters.append([x,y])
+
+
+
+
+
+####################################################################################
+'''Draw function '''
+####################################################################################
+
+def draw() :
+
+  window.fill(WHITE)
+
+  for letter in letters :
+    x , y  = letter #Unpacking letter. Recall each element in looks like [x,y]
+    #drawing each cicle parameters are where, color, start pos, radius, line weight
+    pygame.draw.circle(window, BLACK, (x, y), RADIUS, 3) 
+
+
+  window.blit(images[hangman_status], (100,150))
+  pygame.display.update()
+
+
 
 
 
@@ -48,9 +85,7 @@ clock = pygame.time.Clock()
 
 while run:
   clock.tick(FPS)
-  window.fill(WHITE)
-  window.blit(images[hangman_status], (100,150))
-  pygame.display.update()
+  draw()
 
   #Loop to handle game events
   for event in pygame.event.get():
