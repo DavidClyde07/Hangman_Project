e88789999079bd072ee2514232c1af7a3f85505e
DavidClyde07 <davidclydechetty@gmail.com>
2020-08-12 20:55:09 +0000

    basic finished game but you cant play again

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 import pygame
 import math
-
+import random
 
 pygame.init()  #Initializing the pygame module
 
@@ -34,7 +34,7 @@ for i in range(7):
 ####################################################################################
 '''Game Variables'''
 ####################################################################################
-hangman_status = 1 #tracks the player status hence which image to draw
+hangman_status = 0 #tracks the player status hence which image to draw
 WHITE = (255,255,255)
 RED = (255,0,0)
 BLACK = (0,0,0)
@@ -47,9 +47,15 @@ x_start = round((WIDTH - (RADIUS*2 + GAP) * 13 ) / 2 )
 y_start = 400 
 A = 65 #ASCII code for the letter A
 
+words = ["HANGMAN", "DEVELOPER", "GEOMATICS", ]  #List to store the word to be guessed
+secret_word = random.choice(words)
+guessed = [] #List to store the gussed letters
+
 #Creating a font for the letters, comicsans is the font and 40 is the font size
 LETTER_FONT = pygame.font.SysFont("comicsans", 40)
-
+WORD_FONT = pygame.font.SysFont("comicsans", 60)
+TITLE_FONT = pygame.font.SysFont("comicsans", 70)
+TITLE = "DEVELOPER HANGMAN!"
 
 for i in range(26):
   x = x_start + GAP*2 + ((RADIUS*2 + GAP) * (i % 13))
@@ -64,12 +70,26 @@ for i in range(26):
 def draw() :
 
   window.fill(WHITE)
-  
+  #Draw title 
+  text = TITLE_FONT.render(TITLE, 1, BLACK)
+  window.blit(text, (WIDTH/2 - text.get_width()/2 , 20 ))
+
+  #Drawing a word to be guessed
+  display_word = ""
+
+  for letter in secret_word:
+    if letter in guessed:
+      display_word += letter + " "
+    else:
+      display_word += "_ "
+  text = WORD_FONT.render(display_word, 1, BLACK)
+  window.blit(text, (400,200) )
+
   #Drawing buttons
   for letter in letters :
     x , y, ltr, visible  = letter #Unpacking letter. Recall each element in looks like [x,y, A+i, visible boolean]
     if visible:
-      #drawing each cicle parameters are where, color, start pos, radius, line weight
+      #drawing each cicle. parameters are where, color, start pos, radius, line weight
       pygame.draw.circle(window, BLACK, (x, y), RADIUS, 3) 
       text = LETTER_FONT.render(ltr, 1, BLACK)
       window.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
@@ -78,38 +98,71 @@ def draw() :
   window.blit(images[hangman_status], (100,150))
   pygame.display.update()
 
+####################################################################################
+'''Display Message function '''
+####################################################################################
 
-
-
-
+def display_message(message):
+  pygame.time.delay(1000) #1 sec delay so user can see what happened
+  window.fill(WHITE)
+  text = WORD_FONT.render(message, 1, BLACK)
+  window.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+  pygame.display.update()
+  pygame.time.delay(3000)#3 sec delay so user can read the end message before the game terminates.
+  
 ###################################################################################
 '''Setting up game loop to handel running of the game'''
 ###################################################################################
-FPS = 50 #Game will run at 50 frames per sec, Constant Variable
-run = True 
-clock = pygame.time.Clock()
-
-while run:
-  clock.tick(FPS)
-  draw()
-
-  #Loop to handle game events
-  for event in pygame.event.get():
-    #Handle quiting
-    if event.type == pygame.QUIT:
-      run = False
-    #Handel mouse clicks
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      x_mouse, y_mouse = pygame.mouse.get_pos() #Note pygame coord sys starts at top left
-      #print(pos)
-
-      for letter in letters:
-        x,y,ltr,visible = letter
-        if visible:
-          distance = math.sqrt((x-x_mouse)**2 + (y-y_mouse)**2)
-          #when a letter is clicked make it disappear by setting visible boolean to False
-          if distance < RADIUS:
-            letter[3] = False
 
 
+def main():
+
+  global hangman_status
+  FPS = 50 #Game will run at 50 frames per sec, Constant Variable
+  run = True 
+  clock = pygame.time.Clock()
+  
+  while run:
+    clock.tick(FPS)
+    
+    #Loop to handle game events
+    for event in pygame.event.get():
+      #Handle quiting
+      if event.type == pygame.QUIT:
+        run = False
+      #Handel mouse clicks
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        x_mouse, y_mouse = pygame.mouse.get_pos() #Note pygame coord sys starts at top left
+        #print(pos)
+
+        for letter in letters:
+          x,y,ltr,visible = letter
+          if visible:
+            distance = math.sqrt((x-x_mouse)**2 + (y-y_mouse)**2)
+            #when a letter is clicked make it disappear by setting visible boolean to False
+            if distance < RADIUS:
+              letter[3] = False
+              guessed.append(ltr)
+
+              if ltr not in secret_word:
+                hangman_status += 1
+    
+    draw()
+    
+    won = True
+    for letter in secret_word:
+      if letter not in guessed:
+        won = False
+        break
+    
+    if won:
+      display_message("You WON!!!")
+      break
+
+    if hangman_status == 6:
+      display_message("You LOST!!!")  
+      break
+
+
+main()
 pygame.quit()
